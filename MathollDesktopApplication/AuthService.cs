26446360dd868480e511d3b23e9dece442c87a81
Ólafur Pálsson <olp10@hi.is>
2024-07-24 22:44:32 +0000

    First commit

---
+++ MathollDesktopApplication/AuthService.cs
@@ -0,0 +1,56 @@
+﻿using Auth0.OidcClient;
+using IdentityModel.OidcClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Auth0.OidcClient;
+using System.Windows;
+
+namespace MathollDesktopApplication
+{
+    internal class AuthService
+    {
+        private Auth0Client _client;
+        private static AuthService _instance;
+        public static AuthService Instance => _instance ??= new AuthService();
+
+        public LoginResult LoginResult { get; private set; }
+
+        public void SetLoginResult(LoginResult loginResult)
+        {
+            LoginResult = loginResult;
+        }
+
+        public bool IsLoggedIn => LoginResult != null && !LoginResult.IsError;
+
+        public async void LogIn() 
+        {
+            LoginResult = null;
+            Auth0ClientOptions clientOptions = new Auth0ClientOptions
+            {
+                Domain = "dev-72yh0oy5imbdaqx8.eu.auth0.com",
+                ClientId = "4nF5fHwCq9DXNsO0JLkQBY9rxXfMQcy4",
+                RedirectUri = "myapp://callback"
+            };
+            _client = new Auth0Client(clientOptions);
+            clientOptions.PostLogoutRedirectUri = clientOptions.PostLogoutRedirectUri;
+
+            var loginResult = await _client.LoginAsync();
+
+            if (loginResult.IsError == false)
+            {
+                AuthService.Instance.SetLoginResult(loginResult);
+
+                // Open MainWindow.xaml
+                MainWindow mainWindow = new();
+                mainWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Error: " + loginResult.Error);
+            }
+        }
+    }
+}
---
+++ MathollDesktopApplication/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Auth0.OidcClient;
+
+namespace MathollDesktopApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Auth0Client _client;
+        public MainWindow()
+        {
+            InitializeComponent();
+            CheckLoginStatus();
+
+            if (!AuthService.Instance.IsLoggedIn)
+            {
+                this.Hide();
+                Login login = new();
+                login.Show();
+            }
+        }
+
+        private void CheckLoginStatus()
+        {
+            if (AuthService.Instance.IsLoggedIn)
+            {
+                var user = AuthService.Instance.LoginResult.User;
+                var name = user.FindFirst(c => c.Type == "name")?.Value;
+                var email = user.FindFirst(c => c.Type=="email")?.Value;
+                MessageBox.Show($"Welcome {name} ({email})");
+            }
+            else
+            {
+                MessageBox.Show("User is not logged in");
+            }
+        }
+    }
+}
