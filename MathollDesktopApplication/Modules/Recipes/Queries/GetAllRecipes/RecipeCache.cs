d0f01148c6102662f50e078ff80c8fdd252ca126
Ólafur Pálsson <olp10@hi.is>
2024-07-27 15:55:06 +0000

    Added exchangable content with User Controls to switch between 'tabs' in the menu

---
+++ MathollDesktopApplication/Modules/Recipes/Queries/GetAllRecipes/RecipeCache.cs
@@ -0,0 +1,24 @@
+﻿using MathollDesktopApplication.Modules.Recipes.Queries.GetAllRecipesRequest;
+using System.Threading.Tasks;
+
+namespace MathollDesktopApplication.Modules.Recipes.Queries.GetAllRecipes
+{
+    public static class RecipeCache
+    {
+        private static RecipeDTO[] cachedRecipes;
+
+        public static async Task<RecipeDTO[]> GetRecipes()
+        {
+            if (cachedRecipes == null)
+            {
+                cachedRecipes = await GetAllRecipeDTOs.GetRecipes();
+            }
+            return cachedRecipes;
+        }
+
+        public static void ClearCache()
+        {
+            cachedRecipes = null;
+        }
+    }
+}
---
+++ MathollDesktopApplication/UserControls/SousVideView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace MathollDesktopApplication
+{
+    /// <summary>
+    /// Interaction logic for SousVide.xaml
+    /// </summary>
+    public partial class SousVide : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SousVide"/> class.
+        /// </summary>
+        public SousVide()
+        {
+            InitializeComponent();
+        }
+    }
+}
